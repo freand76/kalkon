143d1f1f86ee4b8f72ee6da0cb42994283820a36
Fredrik Andersson <freand@gmail.com>
2023-12-31 10:17:38 +0100

    Split files in GUI + calculator

--- src/kalkon/__main__.py
+++ src/kalkon/__main__.py
@@ -10,7 +10,7 @@ from PySide6.QtCore import Qt
 from PySide6.QtGui import QIcon, QPainter, QPixmap
 from PySide6.QtWidgets import QApplication
 
-from .kalkon import MainWindow
+from .gui import MainWindow
 
 
 if __name__ == "__main__":
